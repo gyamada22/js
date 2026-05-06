aae2d4605affcf36a896e5c5d26fed84398b63d4
gyamada22 <guilhermeyamada@poli.ufrj.br>
2026-05-06 10:47:58 -0300

    Create Botao.jsx

---
+++ project-01-contador/Botao.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function Botao({ valor, Inc, Dec, Zero, min, max }) {
+  return (
+    <>
+      <h1>Contador</h1>
+      <p>Valor atual é: {valor}</p>
+      <button onClick={Inc} disabled={valor === max}>
+        {" "}
+        Adicionar
+      </button>
+      <button onClick={Dec} disabled={valor === min}>
+        {" "}
+        Diminuir
+      </button>
+      <button onClick={Zero} disabled={valor === 0}>
+        {" "}
+        Zerar
+      </button>
+      {valor === max && <p>Limite máximo atingido</p>}
+      {valor === min && <p>Limite minimo atingido</p>}
+    </>
+  );
+}
